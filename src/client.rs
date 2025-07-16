b1944561046ce1240b1b1643ca10fef8c609da2a
dat58 <vtdat58@gmail.com>
2025-07-16 10:44:03 +0700

    feat: Add timeout option to ClientBuilder

--- src/client.rs
+++ src/client.rs
@@ -57,6 +57,7 @@ pub struct ClientBuilder<D> {
     dst: D,
     username: Option<String>,
     password: Option<String>,
+    timeout: Option<Duration>,
 }
 
 impl<D> ClientBuilder<D>
@@ -70,6 +71,7 @@ where
             dst,
             username: None,
             password: None,
+            timeout: None,
         }
     }
 
@@ -82,9 +84,14 @@ where
         self.password = Some(password.to_owned());
         self
     }
+    
+    pub fn timeout(mut self, timeout: Duration) -> Self {
+        self.timeout = Some(timeout);
+        self
+    }
 
     pub async fn build(self) -> Result<Client> {
-        Client::with_timeout(self.dst, RPC_TIMEOUT, self.username, self.password).await
+        Client::with_timeout(self.dst, self.timeout.unwrap_or(RPC_TIMEOUT), self.username, self.password).await
     }
 }
 
